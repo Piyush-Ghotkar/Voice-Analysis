8846ab98e31c47571f26d302cf79c80bf2a8251c
Piyush Ghotkar <42354510+Piyush-Ghotkar@users.noreply.github.com>
2020-07-04 19:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,24 @@
 
 
 from flask import Flask, jsonify, request
+import pyrebase
 import os as os
 
 
-#obj=__import__("my-voice-analysis")
+from flask import Flask, jsonify, request
+import pickle
 
+
+config={
+    "apiKey": "AIzaSyDeFLJFr2aqcAK40nZOmBtEDNYij49yyAk",
+    "authDomain": "healdon-916dd.firebaseapp.com",
+    "databaseURL": "https://healdon-916dd.firebaseio.com",
+    "projectId": "healdon-916dd",
+    "storageBucket": "healdon-916dd.appspot.com",
+    "messagingSenderId": "756073662506",
+    "appId": "1:756073662506:web:2f4cb2e5e93f1d4b9d1b53"
+}
+firebase=pyrebase.initialize_app(config)
 
 # app
 app = Flask(__name__)
@@ -22,14 +35,15 @@ def predict():
     data = request.get_json(force=True)
 
     path=os.getcwd()
-
+    
+    storage = firebase.storage()
+    storage.child("Audio/27 sec clip.wav").download("downloaded.wav")
     # predictions
     #result = model.predict(data_df)
 
     # send back to browser
     #output = {'results': int(result[0])}
-    output = {'results': path,
-             'input':data}
+    output = {'results': path}
     
     # return data
     return jsonify(results=output)
