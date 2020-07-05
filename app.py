b0f0ea4e3f032816a6c773e95f8ef5e6d4fb26ea
Piyush Ghotkar <42354510+Piyush-Ghotkar@users.noreply.github.com>
2020-07-05 06:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,16 @@
 from flask import Flask, jsonify, request
 import pyrebase
 import os as os
-obj=__import__("my-voice-analysis")
+import parselmouth
+from parselmouth.praat import call, run_file
+import glob
+import pandas as pd
+import numpy as np
+import scipy
+from scipy.stats import binom
+from scipy.stats import ks_2samp
+from scipy.stats import ttest_ind
+import os
 
 from flask import Flask, jsonify, request
 import pickle
@@ -39,7 +48,7 @@ def predict():
     p="27 sec clip"
     c=path
     
-    result=obj.mysptotal(p,c)
+    ar_rate=obj.articulation_rate(p,c)
     
     #storage = firebase.storage()
     #storage.child("Audio/27 sec clip.wav").download("downloaded.wav")
@@ -48,10 +57,27 @@ def predict():
 
     # send back to browser
     #output = {'results': int(result[0])}
-    output = {'results': result}
+    output = {'articulation rate': ar_rate}
     
     # return data
-    return jsonify(results=obj.myspatc(p,c))
+    return jsonify(results=output)
+
+def articulation_rate(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=int(z2[3]) # will be the integer number 10
+        z4=float(z2[3]) # will be the floating point number 8.3
+        print ("articulation_rate=",z3,"# syllables/sec speaking duration")
+    except:
+        z3=0
+        print ("Try again the sound of the audio was not clear")
+    return z3;
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True,use_reloader=False)
