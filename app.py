6b54f3da186f1726fac25cd8bc8b537676cda514
Piyush Ghotkar <42354510+Piyush-Ghotkar@users.noreply.github.com>
2020-07-05 06:20:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,21 +43,28 @@ def predict():
     # get data
     data = request.get_json(force=True)
 
-    path=os.getcwd()
-    
-    p="27 sec clip"
-    c=path
-    
-    ar_rate=articulation_rate(p,c)
-    
     #storage = firebase.storage()
     #storage.child("Audio/27 sec clip.wav").download("downloaded.wav")
-    # predictions
-    #result = model.predict(data_df)
+    
+    
+    p="27 sec clip"
+    c=os.getcwd()
+    
+    ar_rate=articulation_rate(p,c)
+    rate_sph=rate_of_speech(p,c)
+    no_pause=number_of_pauses(p,c)
+    speak_dur=speaking_duration(p,c)
+    org_dur=original_duration(p,c)
+    
+
+  
 
     # send back to browser
-    #output = {'results': int(result[0])}
-    output = {'articulation rate': ar_rate}
+    output = {'articulation rate': ar_rate,
+             'rate of speech': rate_sph,
+             'number of pauses': no_pause,
+             'speking duration':speak_dur,
+             'original duration': org_dur}
     
     # return data
     return jsonify(results=output)
@@ -78,6 +85,75 @@ def articulation_rate(m,p):
         z3=0
         print ("Try again the sound of the audio was not clear")
     return z3;
+
+def rate_of_speech(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=int(z2[2]) # will be the integer number 10
+        z4=float(z2[3]) # will be the floating point number 8.3
+        print ("rate_of_speech=",z3,"# syllables/sec original duration")
+    except:
+        z3=0
+        print ("Try again the sound of the audio was not clear")
+    return z3;
+
+def number_of_pauses(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=int(z2[1]) # will be the integer number 10
+        z4=float(z2[3]) # will be the floating point number 8.3
+        print ("number_of_pauses=",z3)
+    except:
+        z3=0
+        print ("Try again the sound of the audio was not clear")
+    return z3;
+
+def speaking_duration(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=int(z2[3]) # will be the integer number 10
+        z4=float(z2[4]) # will be the floating point number 8.3
+        print ("speaking_duration=",z4,"# sec only speaking duration without pauses")
+    except:
+        z4=0
+        print ("Try again the sound of the audio was not clear")
+    return z4;
+
+
+def original_duration(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=int(z2[3]) # will be the integer number 10
+        z4=float(z2[5]) # will be the floating point number 8.3
+        print ("original_duration=",z4,"# sec total speaking duration with pauses")
+    except:
+        z4=0
+        print ("Try again the sound of the audio was not clear")
+    return z4;
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True,use_reloader=False)
